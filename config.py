68014a60833ac40c4938b4d3590f89b843e99081
chelipika <103322024+chelipika@users.noreply.github.com>
2025-02-26 21:53:29 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 TOKEN = "" # token from BotFather
+USERNAME = "" # Instagram username it | creating a new fresh account is reccomended
+PASSWORD = "" # Instagram password it
 CHANNEL_ID = -100  # замените на ID вашего канала | change ID to the desired channel id
 CHANNEL_LINK = "https://t.me/......."  # ссылка на канал | link/url to the channel | go to channel settings -> invite links -> new link -> set Admin request approval
-
 
 ### note that bot should be in the group and must have admin rights before working
 ### внимание что бот должен быть в группе и иметь права администратора перед началом работы
